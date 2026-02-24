9951288dc332c65d74d14267059e4c81b0cb3487
saidurstartagain <saiduretc@gmail.com>
2026-02-24 14:43:56 +0600

    header id get with js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Empty array for interview and rejected list 
+let interviewList = [];
+let rejectedList = [];
+let currentStatus = 'all';
+
+
+// get element by ID of Header part  
+const totalJob = document.getElementById("total-job");
+const totalJob2 = document.getElementById("total-job-2");
+
+
