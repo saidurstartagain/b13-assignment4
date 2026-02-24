763d4b2eb502f4c847e93ae326bc107572023092
saidurstartagain <saiduretc@gmail.com>
2026-02-24 14:54:24 +0600

    Main function added

--- script.js
+++ script.js
@@ -123,3 +123,80 @@ function toggleStyle(id) {
 
 }
 
+
+// get main container 
+const mainContainer = document.querySelector("main");
+mainContainer.addEventListener('click', function (event) {
+    // console.log(event.target);
+    if (event.target.classList.contains('btn-interview')) {
+        // console.log(event.target.classList.contains('btn-interview'))
+        const parentNode = event.target.parentNode.parentNode;
+        const mobileFast = parentNode.querySelector('.mobileFast').innerText;
+        const remoteFull = parentNode.querySelector('.remoteFull').innerText;
+        const reactNative = parentNode.querySelector('.reactNative').innerText;
+        const build = parentNode.querySelector('.build').innerText;
+        const status = parentNode.querySelector('.status-btn').innerText;
+        parentNode.querySelector('.status-btn').innerText = "interview";
+        const cardInfo = {
+            mobileFast,
+            remoteFull,
+            reactNative,
+            build,
+            status: 'interview'
+
+        }
+        // console.log(cardInfo)
+        // console.log(interviewList)
+        const jobExist = interviewList.find(item => item.mobileFast == cardInfo.mobileFast)
+        // console.log(jobExist)
+
+        if (!jobExist) {
+            interviewList.push(cardInfo);
+            // console.log("from exit")
+        }
+        rejectedList = rejectedList.filter(item => item.mobileFast != cardInfo.mobileFast)
+        totalCount()
+        // console.log(interviewList)
+        // console.log(rejectedList)
+
+        if (currentStatus == "btn-rejected-toggle") {
+            renderRejection()
+        }
+        // console.log(currentStatus)
+    }
+    else if (event.target.classList.contains('btn-rejected')) {
+        // console.log('hello from rejected job')
+        const parentNode = event.target.parentNode.parentNode;
+        const mobileFast = parentNode.querySelector('.mobileFast').innerText;
+        const remoteFull = parentNode.querySelector('.remoteFull').innerText;
+        const reactNative = parentNode.querySelector('.reactNative').innerText;
+        const build = parentNode.querySelector('.build').innerText;
+        const status = parentNode.querySelector('.status-btn').innerText;
+        parentNode.querySelector('.status-btn').innerText = "Rejected";
+        const cardInfo = {
+            mobileFast,
+            remoteFull,
+            reactNative,
+            build,
+            status: 'Rejected'
+
+        }
+        // console.log(cardInfo)rejectedList
+        const jobExist = rejectedList.find(item => item.mobileFast == cardInfo.mobileFast)
+        // console.log(jobExist)
+        if (!jobExist) {
+            rejectedList.push(cardInfo);
+            // console.log("from exit")
+        }
+        console.log(rejectedList)
+        interviewList = interviewList.filter(item => item.mobileFast != cardInfo.mobileFast)
+        // totalCount()
+        totalCount()
+        if (currentStatus == "btn-interview-toggle") {
+            renderInterview()
+        }
+
+    }
+})
+
+// Filter section rending and document get ..
